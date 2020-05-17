bcf1a39863c89520aae9f6e49861e1a257cbb8ea
Onon <gantumuronon539@gmail.com>
2020-05-17 13:02:24 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsForm_Drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(427, 92);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(286, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.addColor_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm_Drawing
+{
+    public partial class Form1 : Form
+    {
+        float startAngle = 0.0F;
+        float sweepAngle = 0.0F;
+        List<Color> colors;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+
+            colors = new List<Color>();
+            colors.Add(Color.FromArgb(255, 0, 0, 0));
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics gr = e.Graphics;
+
+            Rectangle rect = new Rectangle(100, 100 , 200, 200);
+            float grad = sweepAngle / colors.Count;
+            for(int i = 0; i <colors.Count; i++) { 
+                Pen pen = new Pen(colors[i], 10);
+                gr.DrawArc(pen, rect, startAngle + grad*i, grad*i + grad);
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = "" + trackBar1.Value + "%";
+            sweepAngle = trackBar1.Value * 360 / 100;
+            Invalidate();
+        }
+
+        private void addColor_Click(object sender, EventArgs e)
+        {
+            Color c = colors.Last();
+            int r = c.R + 20;
+            int g = c.R + 20;
+            int b = c.R + 20;
+            if (r > 255) r = 255;
+            if (g > 255) g = 255;
+            if (b > 255) b = 255;
+            colors.Add(Color.FromArgb(255, r, g, b));
+            Invalidate();
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if(colors.Count > 1)colors.Remove(colors.Last());
+            Invalidate();
+        }
+    }
+}
